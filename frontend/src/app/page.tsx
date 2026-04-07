ab05108e8586ede2d69a8e3f7fc8800a7f634f51
Nguyễn Duy Tường <0023414143@student.dthu.edu.vn>
2026-04-07 17:35:20 +0700

    cap nhat page

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import UserLayout from "./(user)/layout";
+import UserHomePage from "./(user)/page";
+
+export default function HomePage() {
+	return (
+		<UserLayout>
+			<UserHomePage />
+		</UserLayout>
+	);
+}
